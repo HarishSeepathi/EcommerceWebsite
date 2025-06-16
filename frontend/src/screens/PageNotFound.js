d97663da9427d26a4b683b9fb9fee4057d3b0917
HarishSeepathi <seepathiharish0105@gmail.com>
2025-06-16 16:21:36 +0530

    first commit

---
+++ frontend/src/screens/PageNotFound.js
@@ -0,0 +1,11 @@
+import './PageNotFound.css';
+
+const PageNotFound = () => {
+    return (
+        <div className='pageNotfound'>
+            PageNotFound
+        </div>
+    );
+}
+
+export default PageNotFound;
